7cf9893ab8f45680de13ae129dd2159c7892bbf9
Raphael <newsist@naver.com>
2024-04-23 16:00:05 +0900

    Port Change

--- snowball.py
+++ snowball.py
@@ -12,7 +12,7 @@ def index():
     return render_template("index.jsp")
 
 def main():
-    app.run(host='0.0.0.0', debug=False, port=8001)
+    app.run(host='0.0.0.0', debug=False, port=5000)
     #app.run(host='127.0.0.1', debug=False, port=8001)
 
 @app.route('/link0')
